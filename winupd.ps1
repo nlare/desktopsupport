ddf3b88a5417fab7e56bdbe442ba0a1668b3d979
Nathan Lare <96361319+nlare@users.noreply.github.com>
2023-08-23 17:55:29 -0500

    Add files via upload

---
+++ winupd.ps1
@@ -0,0 +1,33 @@
+﻿<#
+You will need to install the PSWindows using the following:
+
+
+Install-Module PSWindowsUpdate
+>> Add-WUServiceManager -MicrosoftUpdate
+#>
+
+#Testing for Admin
+
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+'Running with full priveleges'
+
+#Force Windows Updates
+
+Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot | Out-File "C:\($env.computername-Get-Date -f yyyy-MM-dd)-MSUpdates.log" -Force
+Read-Host "This is the end of PSWindowsUpdate"
+PAUSE
